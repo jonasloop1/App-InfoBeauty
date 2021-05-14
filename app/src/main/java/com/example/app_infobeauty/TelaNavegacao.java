484912a8eb86ee735689b9e2155d53d74ad8116e
Jonas <61604557+jonasloop1@users.noreply.github.com>
2021-05-14 10:17:28 -0300

    criação do menu lateral junto com o menu de navegação horizontal.

--- app/src/main/java/com/example/app_infobeauty/TelaNavegacao.java
+++ app/src/main/java/com/example/app_infobeauty/TelaNavegacao.java
@@ -1,15 +1,27 @@
 package com.example.app_infobeauty;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.Menu;
 import android.widget.Toast;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.app_infobeauty.databinding.ActivityTelaNavegacaoBinding;
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class TelaNavegacao extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityTelaNavegacaoBinding binding;
     private final int ID_Home = 1;
     private final int ID_Search = 2;
     private final int ID_Calendar = 3;
@@ -18,9 +30,22 @@ public class TelaNavegacao extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_navegacao);
 
-        TextView selected_page = findViewById(R.id.selected_pagina);
+        binding = ActivityTelaNavegacaoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarTelaNavegacao.toolbar);
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_tela_navegacao);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
         MeowBottomNavigation bottomNavigation = findViewById(R.id.bottonNavigation);
 
         bottomNavigation.add(new MeowBottomNavigation.Model(ID_Home, R.drawable.ic_nav_home));
@@ -42,24 +67,38 @@ public class TelaNavegacao extends AppCompatActivity {
                 String name;
                 switch (item.getId()){
                     case ID_Home: name = "Home";
-                    break;
+                        break;
 
                     case ID_Search: name = "Search";
-                    break;
+                        break;
 
                     case ID_Calendar: name = "Calendar";
-                    break;
+                        break;
 
                     case ID_Profile: name = "Profile";
-                    break;
+                        break;
 
                     default: name = "";
                 }
-                selected_page.setText(getString(R.string.navegacao_page_selected, name));
+                //selected_page.setText(getString(R.string.navegacao_page_selected, name));
             }
         });
 
         bottomNavigation.setCount(ID_Profile, "+");
         bottomNavigation.show(ID_Home, true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.tela_navegacao, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_tela_navegacao);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
