6dfa288f3d2fe21697231b3e51db96500cbc8ba5
InfoBeaty <viniciuspinheiro457@gmail.com>
2021-05-23 20:31:49 -0300

    Criada a base de dados "Usuario" com as colunas id, nome, email e senha. e feito class para inserir no banco esses dados.

--- app/src/main/java/com/example/app_infobeauty/TelaCadastroUsuario.java
+++ app/src/main/java/com/example/app_infobeauty/TelaCadastroUsuario.java
@@ -2,9 +2,14 @@ package com.example.app_infobeauty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.Serializable;
 
 public class TelaCadastroUsuario extends AppCompatActivity {
 
@@ -12,11 +17,67 @@ public class TelaCadastroUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro_usuario);
+
+        Button btnSalvar = (Button)findViewById(R.id.cadastrarUsuario);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //carregando os campos
+                EditText txtNome = (EditText)findViewById(R.id.nomeUsuario);
+                EditText txtEmail = (EditText)findViewById(R.id.emailUsuario);
+                EditText txtSenha = (EditText)findViewById(R.id.senhaUsuario);
+
+                //pegando os valores
+                String nome = txtNome.getText().toString();
+                String email = txtEmail.getText().toString();
+                String senha = txtSenha.getText().toString();
+
+                //salvando os dados
+                UsuarioDAO dao = new UsuarioDAO(getBaseContext());
+                boolean sucesso = dao.salvar(nome, email, senha);
+                if(sucesso) {
+                    //limpa os campos
+                    txtNome.setText("");
+                    txtEmail.setText("");
+                    txtSenha.setText("");
+
+                    Snackbar.make(view, "Salvou!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }else{
+                    Snackbar.make(view, "Erro ao salvar, consulte os logs!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            }
+        });
+
     }
+    public static class Usuario implements Serializable {
 
-    public void telaCadastroUsuarioNavegacao(View view){
+        private int id;
+        private String nome;
+        private String email;
+        private String senha;
 
-        Intent intent = new Intent(this, TelaNavegacao.class);
-        startActivity(intent);
+        public Usuario(int id, String nome, String email, String senha){
+            this.id = id;
+            this.nome = nome;
+            this.email = email;
+            this.senha = senha;
+        }
+
+        public int getId(){ return this.id; }
+        public String getNome(){ return this.nome; }
+        public String getEmail(){ return this.email; }
+        public String getSenha(){ return this.senha; }
+
+        @Override
+        public boolean equals(Object o){
+            return this.id == ((Usuario)o).id;
+        }
+
+        @Override
+        public int hashCode(){
+            return this.id;
+        }
     }
 }
---
+++ app/src/main/java/com/example/app_infobeauty/UsuarioDAO.java
@@ -0,0 +1,39 @@
+package com.example.app_infobeauty;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioDAO {
+
+    private final String TABLE_USUARIO = "Usuario";
+    private DbGateway gw;
+
+    public UsuarioDAO(Context ctx){
+        gw = DbGateway.getInstance(ctx);
+    }
+
+    public boolean salvar(String nome, String email, String senha) {
+        ContentValues cv = new ContentValues();
+        cv.put("Nome", nome);
+        cv.put("Email", email);
+        cv.put("Senha", senha);
+        return gw.getDatabase().insert(TABLE_USUARIO, null, cv) > 0;
+    }
+
+    public List<TelaCadastroUsuario.Usuario> retornarTodos(){
+        List<TelaCadastroUsuario.Usuario> usuario = new ArrayList<>();
+        Cursor cursor = gw.getDatabase().rawQuery("SELECT * FROM Usuario", null);
+        while(cursor.moveToNext()){
+            int id = cursor.getInt(cursor.getColumnIndex("ID"));
+            String nome = cursor.getString(cursor.getColumnIndex("Nome"));
+            String email = cursor.getString(cursor.getColumnIndex("Email"));
+            String senha = cursor.getString(cursor.getColumnIndex("Senha"));
+            usuario.add(new TelaCadastroUsuario.Usuario(id, nome, email, senha));
+        }
+        cursor.close();
+        return usuario;
+    }
+}
