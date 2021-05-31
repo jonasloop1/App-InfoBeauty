10c472480afa59c40dd90a93a0e48be05bcbdbb0
MarquesRJ <gabrielmarques142@gmail.com>
2021-05-31 18:56:55 -0300

    Permissão INTERNET para integração do Mapa com o smartphone Mudança da classe Maps_InfoBeauty de privada para publica funcionalidade zoom adicionado ao mapa

--- app/src/main/java/com/example/app_infobeauty/Maps_InfoBeauty.java
+++ app/src/main/java/com/example/app_infobeauty/Maps_InfoBeauty.java
@@ -15,31 +15,24 @@ public class Maps_InfoBeauty extends FragmentActivity implements OnMapReadyCallb
     private GoogleMap mMap;
 
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    public void onCreate( Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.fragment_location);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+//Coordenadas para a Cidade do Rio de Janeiro
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Rio = new LatLng(-22.2752762, -42.419415);
+        mMap.addMarker(new MarkerOptions().position(Rio).title("Marcado em Rio de Janeiro"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Rio));
     }
 }
